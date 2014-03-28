61f146a2654103e8b83e5a438ec14377aa5d6730
Jorge Quilcate <quilcate.jorge@gmail.com>
2014-03-28 00:04:12 -0500

    SOA 11.1.1.7 installation

--- oracle-soa-suite/attributes/install-soa-11.1.1.7.rb
+++ oracle-soa-suite/attributes/install-soa-11.1.1.7.rb
@@ -1,11 +1,24 @@
 default["soa_installation"]["middleware_home"] = "C:/u01/oracle/fmw/soabpm/11.1.1.7"
 
+default["soa_installation"]["oracle_soa_home"] = "C:/u01/oracle/fmw/soabpm/11.1.1.7/oracle_soa"
+
 default["soa_installation"]["java_home"] = "C:/oracle/java/jdk1.7.0_51"
+
+default["soa_installation"]["soa_tmp_dir"] = "C:/u01/oracle/fmw/soabpm/11.1.1.7/tmp/soa"
 
 default["soa_installation"]["directories"] = [
 	"C:/u01",
 	"C:/u01/oracle",
 	"C:/u01/oracle/fmw",
 	"C:/u01/oracle/fmw/soabpm",
-	"C:/u01/oracle/fmw/soabpm/11.1.1.7"
+	"C:/u01/oracle/fmw/soabpm/11.1.1.7",
+	"C:/u01/oracle/fmw/soabpm/11.1.1.7/tmp",
+	"C:/u01/oracle/fmw/soabpm/11.1.1.7/tmp/soa"
 ]
+
+default["soa_installation"]["installer_zips"] = [
+	"C:/oracle/fmw/installers/soa/ofm_soa_generic_11.1.1.7.0_disk1_1of2.zip",
+	"C:/oracle/fmw/installers/soa/ofm_soa_generic_11.1.1.7.0_disk1_2of2.zip"
+]
+
+default["soa_installation"]["user"] = "JEQO"
--- oracle-soa-suite/recipes/install-soa-11.1.1.7.rb
+++ oracle-soa-suite/recipes/install-soa-11.1.1.7.rb
@@ -13,4 +13,58 @@ node["soa_installation"]["directories"].each do |soa_directory|
 	end
 end
 
+##
+# Unzip installer
+##
 
+node["soa_installation"]["installer_zips"].each do |zip_file|
+	unzip_command = "unzip -o -d " + node["soa_installation"]["soa_tmp_dir"] + " " + zip_file
+	execute unzip_command do
+		action :run
+		not_if do 
+			FileTest.directory?(node["soa_installation"]["soa_tmp_dir"] + "/Disk4") 
+		end	
+	end
+end
+	
+##
+# Prepare files
+##
+
+# Response files
+tmp_directory = node["soa_installation"]["middleware_home"] + "/tmp"
+oracle_soa_response = tmp_directory + "/oracle_soa_response.xml"
+
+template oracle_soa_response do
+	source "oracle_soa_response.erb"
+	rights :full_control, user
+	variables({
+		:middleware_home => node["soa_installation"]["middleware_home"],
+		:oracle_soa_home => node["soa_installation"]["oracle_soa_home"]
+	})
+end
+
+# Install script
+run_command = node["soa_installation"]["soa_tmp_dir"] + "/Disk1/install/win64/setup.exe -jreLoc " + node["soa_installation"]["java_home"] + " -silent -response " + oracle_soa_response
+
+##
+# Run installer
+##
+
+execute run_command do
+	action :run
+	not_if do 
+		FileTest.directory?(node["soa_installation"]["oracle_soa_home"]) 
+	end
+end
+
+##
+# Clean
+##
+
+# Delete temp directory
+directory tmp_directory do
+	recursive true
+	rights :full_control, user
+	action :delete
+end
